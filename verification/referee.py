d09bc2301e5b733069f555868bd3251c32906f55
Oleksandr Zozulia <freemanlex@gmail.com>
2023-12-10 19:48:01 +0200

    "first commit"

---
+++ verification/referee.py
@@ -0,0 +1,23 @@
+from checkio.signals import ON_CONNECT
+from checkio import api
+from checkio.referees.io_template import CheckiOReferee
+# from checkio.referees.checkers import to_list
+
+from tests import TESTS
+
+api.add_listener(
+    ON_CONNECT,
+    CheckiOReferee(
+        tests=TESTS,
+        # checker=to_list,
+        function_name={
+            "python": "postfix_evaluate",
+            "js": "postfixEvaluate"
+        },
+        cover_code={
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
+        }
+    ).on_ready)
---
+++ verification/tests.py
@@ -0,0 +1,36 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": [[2, 3, '+', 4, '*']],
+            "answer": 20,
+        },
+        {
+            "input": [[2, 3, 4, '*', '+']],
+            "answer": 14,
+        },
+        {
+            "input": [[3, 3, 3, '-', '/', 42, '+']],
+            "answer": 42,
+        },
+    ],
+    "Extra": [
+        {
+            "input": [[7, 3, '/']],
+            "answer": 2,
+        },
+        {
+            "input": [[1, 2, 3, 4, 5, '*', '*', '*', '*']],
+            "answer": 120,
+        },
+    ]
+}
